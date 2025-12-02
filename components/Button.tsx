cf7919e8995f1d15ce92865bf548b84bffc5f9d5
Jean Desauw <78590821+JeanDes-Code@users.noreply.github.com>
2025-12-02 15:13:59 +0100

    adding '@expo-google-fonts/poppins'

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { TouchableOpacity, Text, StyleSheet } from "react-native";
 
+import {
+  useFonts, Poppins_400Regular
+} from '@expo-google-fonts/poppins';
+
 interface MyButtonProps {
   onPress: () => void;
   text: string;
 }
 
 export const MyButton = ({ onPress, text }: MyButtonProps) => {
+  
+  const [fontsLoaded] = useFonts({Poppins_400Regular});
+  if (!fontsLoaded) {
+    return <Text>Loading fonts...</Text>;
+  }
+
   return (
     <TouchableOpacity style={styles.container} onPress={onPress}>
       <Text style={styles.text}>{text}</Text>
@@ -22,5 +32,5 @@ const styles = StyleSheet.create({
     alignSelf: "flex-start",
     borderRadius: 8,
   },
-  text: { color: "white", fontSize: 16, fontWeight: "bold" },
+  text: { color: "white", fontSize: 16, fontWeight: "bold", fontFamily: "Poppins_400Regular" },
 });
